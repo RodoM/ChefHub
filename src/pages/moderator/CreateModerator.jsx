0c99d83ebdfbe5e29a7bcf454c20dd0af276ead7
JensonMedina <medinajenson74@gmail.com>
2024-09-20 09:58:05 -0300

    Agregadas validaciones al formulario de crear usuario

--- src/pages/moderator/CreateModerator.jsx
+++ src/pages/moderator/CreateModerator.jsx
@@ -11,10 +11,123 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { Textarea } from "@/components/ui/textarea";
+import { useRef, useState } from "react";
+
 const CreateModerator = () => {
+  //Inicio definicion de variables de formulario para validacion de campos requeridos
+
+  //refs
+  const fullNameRef = useRef();
+  const emailRef = useRef();
+  const urlPhotoRef = useRef();
+  const rolRef = useRef();
+  const descriptionRef = useRef();
+  const passwordRef = useRef();
+  const confirmPasswordRef = useRef();
+
+  //estados formulario
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [urlPhoto, setUrlPhoto] = useState("");
+  const [rol, setRol] = useState("");
+  const [description, setDescription] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  //estados error
+  const [errors, setErrors] = useState({});
+
+  //fin definicion de variables de formulario para validacion de campos requeridos
+
+  //funcion para enviar el formulario
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!validateForm()) return;
+
+    //este es el nuevo usuario
+    const newUser = {
+      fullName,
+      email,
+      urlPhoto,
+      rol,
+      description,
+      password,
+    };
+
+    console.log(newUser);
+    alert("Usuario creado correctamente");
+
+    //resetear formulario
+    setFullName("");
+    setEmail("");
+    setUrlPhoto("");
+    setRol("");
+    setDescription("");
+    setPassword("");
+    setConfirmPassword("");
+
+    //resetar estados error
+    setErrors({});
+  };
+
+  //funcion para validar los campos del formulario
+  const validateForm = () => {
+    const newErrors = {};
+    if (fullName.trim() === "") {
+      newErrors.fullName = "Por favor, rellena el nombre y apellido";
+      fullNameRef.current.focus();
+      setErrors(newErrors);
+      return;
+    }
+    if (email.trim() === "") {
+      newErrors.email = "Por favor, rellena el email";
+      emailRef.current.focus();
+      setErrors(newErrors);
+      return;
+    }
+    if (urlPhoto.trim() === "") {
+      newErrors.urlPhoto = "Por favor, rellena la URL de la foto";
+      urlPhotoRef.current.focus();
+      setErrors(newErrors);
+      return;
+    }
+    if (rol.trim() === "") {
+      newErrors.rol = "Por favor, selecciona un rol";
+      rolRef.current.focus();
+      setErrors(newErrors);
+      return;
+    }
+    if (description.trim() === "") {
+      newErrors.description = "Por favor, rellena la descripción";
+      descriptionRef.current.focus();
+      setErrors(newErrors);
+      return;
+    }
+    if (password.trim() === "") {
+      newErrors.password = "Por favor, rellena la contraseña";
+      passwordRef.current.focus();
+      setErrors(newErrors);
+      return;
+    }
+    if (confirmPassword.trim() === "") {
+      newErrors.confirmPassword = "Por favor, confirma la contraseña";
+      confirmPasswordRef.current.focus();
+      setErrors(newErrors);
+      return;
+    }
+    if (password !== confirmPassword) {
+      newErrors.confirmPassword = "Las contraseñas no coinciden";
+      confirmPasswordRef.current.focus();
+      setErrors(newErrors);
+      return;
+    }
+
+    return true;
+  };
+
   return (
-    <div className="flex justify-center w-full mt-10 p-8">
-      <form className="w-full max-w-7xl">
+    <div className="flex justify-center w-full my-10">
+      <form className="w-full max-w-7xl" onSubmit={handleSubmit}>
         <h2 className="text-left text-2xl font-bold mb-8">Nuevo Usuario</h2>
 
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mb-8">
@@ -25,7 +138,13 @@ const CreateModerator = () => {
               type="text"
               className="w-full"
               placeholder="Ingrese el nombre del usuario"
+              ref={fullNameRef}
+              onChange={(e) => setFullName(e.target.value)}
+              value={fullName}
             />
+            {errors.fullName && (
+              <p className="text-red-500">{errors.fullName}</p>
+            )}
           </div>
           <div>
             <Label htmlFor="email">Email</Label>
@@ -34,7 +153,11 @@ const CreateModerator = () => {
               type="email"
               className="w-full"
               placeholder="Ingrese el email del usuario"
+              ref={emailRef}
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
             />
+            {errors.email && <p className="text-red-500">{errors.email}</p>}
           </div>
         </div>
 
@@ -46,12 +169,22 @@ const CreateModerator = () => {
               type="text"
               className="w-full"
               placeholder="Ingrese la URL de la foto del usuario"
+              ref={urlPhotoRef}
+              onChange={(e) => setUrlPhoto(e.target.value)}
+              value={urlPhoto}
             />
+            {errors.urlPhoto && (
+              <p className="text-red-500">{errors.urlPhoto}</p>
+            )}
           </div>
           <div>
             <Label htmlFor="rol">Rol</Label>
-            <Select id="rol">
-              <SelectTrigger className="w-full">
+            <Select
+              id="rol"
+              onValueChange={(value) => setRol(value)}
+              value={rol}
+            >
+              <SelectTrigger className="w-full" ref={rolRef}>
                 <SelectValue placeholder="Elija un rol" />
               </SelectTrigger>
               <SelectContent>
@@ -62,6 +195,7 @@ const CreateModerator = () => {
                 </SelectGroup>
               </SelectContent>
             </Select>
+            {errors.rol && <p className="text-red-500">{errors.rol}</p>}
           </div>
         </div>
 
@@ -71,7 +205,13 @@ const CreateModerator = () => {
             id="description"
             placeholder="Ingrese una breve descripción para el usuario"
             className=" w-full resize-none"
+            ref={descriptionRef}
+            onChange={(e) => setDescription(e.target.value)}
+            value={description}
           />
+          {errors.description && (
+            <p className="text-red-500">{errors.description}</p>
+          )}
         </div>
 
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mb-8">
@@ -82,7 +222,13 @@ const CreateModerator = () => {
               type="password"
               className="w-full"
               placeholder="Ingrese la contraseña del usuario"
+              ref={passwordRef}
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
             />
+            {errors.password && (
+              <p className="text-red-500">{errors.password}</p>
+            )}
           </div>
           <div>
             <Label htmlFor="confirmPassword">Confirmar Contraseña</Label>
@@ -91,7 +237,13 @@ const CreateModerator = () => {
               type="password"
               className="w-full"
               placeholder="Confirme la contraseña del usuario"
+              ref={confirmPasswordRef}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              value={confirmPassword}
             />
+            {errors.confirmPassword && (
+              <p className="text-red-500">{errors.confirmPassword}</p>
+            )}
           </div>
         </div>
 
