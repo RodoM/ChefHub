e14480e2b5d1c0e3859524499bdcf040678be717
JensonMedina <medinajenson74@gmail.com>
2024-10-31 10:28:30 -0300

    Se implementó un boton en la vista de administrador para la navegación de creación de usuario

--- src/routes/AdminProtected.jsx
+++ src/routes/AdminProtected.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import { useContext } from "react";
 import { AuthenticationContext } from "../services/authentication/AuthenticationContext";
 import { Navigate } from "react-router-dom";
+import { ADMIN } from "@/constants/constants";
 
 const AdminProtected = ({ children }) => {
   const { user } = useContext(AuthenticationContext);
   if (!user) {
     return <Navigate to="/login" replace />;
   }
-  if (user.role !== "Admin") {
+  if (user.role !== ADMIN) {
     return <Navigate to="/" replace />;
   }
 
